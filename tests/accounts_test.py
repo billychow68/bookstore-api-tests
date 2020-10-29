d77beb4b546a001b3911fb4a2cfb8246a996d963
Billy <billchow07@gmail.com>
2020-10-28 20:18:54 -0700

    Misc updates.

--- tests/accounts_test.py
+++ tests/accounts_test.py
@@ -1,51 +1,71 @@
 from tests.base_test import BaseTest
 import pytest
-import json
 import uuid
 
 
-class TestAccounts(BaseTest):
+class TestAccountsAPI(BaseTest):
 
-    def test_create_account_with_valid_password(self):
-        user = self.new_user()
-        res = self.create_user(user)
-        assert res["status_code"] == 201
+    def test_create_user_with_valid_input(self):
+        user = self.generate_username_password()
+        resp = self.create_user(user)
+        resp_body = resp.json()
+        assert resp.status_code == 201
+        assert resp_body["username"] == user["userName"]
+        assert resp_body["userID"] != ""
         pass
 
-    data1 = [["user123", "abcdefgh", 400, 26],
-             ["user123", "12345678", 400, 26],
-             ["user123", "ABCDEFGH", 400, 26],
-             ["user123", "aBcDeFgH", 400, 26],
-             ["user123", "", 400, -1],
-             ["user123", "abcd1234", 400, 26],
-             ["user123", "aBcD1234", 400, 26],
-             ["user123", "!@#$%^&*", 400, 26],
-             ["user123", "Test1234", 400, 26],
-             ["", "Test!234", 400, -1],
-             ["", "", 400, -1]]
+    data1 = [[{"userName": "user123", "password": "abcdefgh"}, 400, 26],
+             [{"userName": "user123", "password": "12345678"}, 400, 26],
+             [{"userName": "user123", "password": "ABCDEFGH"}, 400, 26],
+             [{"userName": "user123", "password": "aBcDeFgH"}, 400, 26],
+             [{"userName": "user123", "password": ""}, 400, -1],
+             [{"userName": "user123", "password": "Test!23"}, 400, 26],
+             [{"userName": "user123", "password": "abcd1234"}, 400, 26],
+             [{"userName": "user123", "password": "aBcD1234"}, 400, 26],
+             [{"userName": "user123", "password": "!@#$%^&*"}, 400, 26],
+             [{"password": "!@#$%^&*"}, 400, -1],
+             [{"userName": "user123"}, 400, -1],
+             [{"userName": "user123", "password": "Test1234"}, 400, 26],
+             [{"userName": "", "password": "Test!234"}, 400, -1],
+             [{}, 400, -1],
+             [{"userName": "", "password": ""}, 400, -1]]
 
-    @pytest.mark.parametrize("user, password, status_code, len", data1)
-    def test_create_account_with_invalid_password(self, user, password, status_code, len):
-        res = self.create_user([user, password])
-        assert res["status_code"] == status_code
-        assert res["text"].find("Passwords must have at least one non alphanumeric character") == len
+    @pytest.mark.parametrize("user, status_code, len_", data1)
+    def test_create_user_with_invalid_input(self, user, status_code, len_):
+        resp = self.create_user(user)
+        assert resp.status_code == status_code
+        assert resp.text.find("Passwords must have at least one non alphanumeric character") == len_
         pass
 
-    def test_delete_existing_account(self):
-        user = self.new_user()
-        res = self.create_user(user)
-        assert res["status_code"] == 201
-        temp = json.loads(res["text"])
-        res = self.delete_user_basic_auth(temp["userID"], user[0], user[1])
-        assert res["status_code"] == 204
+    def test_delete_existing_user_basic_auth(self):
+        user = self.generate_username_password()
+        resp = self.create_user(user)
+        assert resp.status_code == 201
+        resp_body = resp.json()
+        resp = self.delete_user_basic_auth(resp_body["userID"], user)
+        assert resp.status_code == 204
         pass
 
-    def test_delete_account_with_invalid_uuid(self):
-        user = self.new_user()
-        res = self.create_user(user)
-        assert res["status_code"] == 201
-        id = str(uuid.uuid4())
-        res = self.delete_user_basic_auth(id, user[0], user[1])
-        assert res["status_code"] == 200
-        assert res["text"] == '{"code":"1207","message":"User Id not correct!"}'
+    def test_delete_existing_user_token(self):
+        user = self.generate_username_password()
+        resp1 = self.create_user(user)
+        assert resp1.status_code == 201
+        resp_body1 = resp1.json()
+        uuid_ = resp_body1["userID"]
+        resp2 = self.generate_token(user)
+        assert resp2.status_code == 200
+        resp_body2 = resp2.json()
+        token = resp_body2["token"]
+        resp3 = self.delete_user_token(uuid_, token)
+        assert resp3.status_code == 204
+        pass
+
+    def test_delete_user_with_invalid_uuid_basic_auth(self):
+        user = self.generate_username_password()
+        response = self.create_user(user)
+        assert response.status_code == 201
+        uuid_ = str(uuid.uuid4())
+        response = self.delete_user_basic_auth(uuid_, user)
+        assert response.status_code == 200
+        assert response.text == '{"code":"1207","message":"User Id not correct!"}'
         pass
--- tests/base_test.py
+++ tests/base_test.py
@@ -1,37 +1,42 @@
 import requests
 from requests.auth import HTTPBasicAuth
 import uuid
+import json
 
 
 class BaseTest:
 
     base_url = "https://demoqa.com"
 
-    def new_user(self):
-        id = str(uuid.uuid4()).split('-',1)[0].rstrip()
-        username = "user-" + str(id)
-        password = "P@ss-" + str(id)
-        print(username, password)
-        return [username, password]
-
-    def delete_user_basic_auth(self, uuid, user, password):
-        url = self.base_url + '/Account/v1/User/' + uuid
-        auth = HTTPBasicAuth(user, password)
-        r = requests.delete(url, auth=auth)
-        return dict({"status_code": r.status_code, "ok": r.ok, "text": r.text})
-
-    # def delete_user_token(uuid, token):
-    #     url = base_url + '/Account/v1/User/' + uuid
-    #     headers = f'\{"Authorization": "Bearer {token}"\}'
-    #     r = requests.delete(url, headers)
-    #     pass
+    def generate_username_password(self):
+        """This method generates a random username and password and returns the data as a dict."""
+        uuid_ = str(uuid.uuid4()).split('-', 1)[0].rstrip()
+        username = "user-" + str(uuid_)
+        password = "P@ss-" + str(uuid_)
+        return {"userName": username, "password": password}
 
     def create_user(self, user):
+        """This method calls the accounts create user endpoint and returns the response object."""
         url = self.base_url + '/Account/v1/User'
-        payload = dict({"userName": user[0], "password": user[1]})
-        # headers = {'Content-Type':'application/json', 'Accept':'application/json'}
-        r = requests.post(url, data=payload)
-        return dict({"status_code": r.status_code, "ok": r.ok, "text": r.text})
+        headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+        payload = user
+        return requests.post(url, data=payload, json=headers)
 
+    def delete_user_basic_auth(self, uuid_, user):
+        """This method calls the deletion endpoint using basic auth and returns the response object."""
+        url = self.base_url + '/Account/v1/User/' + uuid_
+        auth = HTTPBasicAuth(user["userName"], user["password"])
+        headers = {'Accept': "application/json"}
+        return requests.delete(url, auth=auth, json=headers)
 
+    def delete_user_token(self, uuid_, token):
+        """This method calls the user deletion endpoint using a token and returns the response object. """
+        url = self.base_url + '/Account/v1/User/' + uuid_
+        headers = '{"Authorization": "Bearer ' + token + '", "Accept": "application/json"}'
+        return requests.delete(url, headers=json.loads(headers))
 
+    def generate_token(self, user):
+        """This method calls the generate token endpoing and returns the response object."""
+        url = self.base_url + '/Account/v1/GenerateToken'
+        # todo: add headers
+        return requests.post(url, user)
