c7b198275ad17448cced43fb1f92919dc3dbf499
Billy <billchow07@gmail.com>
2020-10-31 09:12:36 -0700

    Ensure logs get printed on assertions.

--- tests/account_test.py
+++ tests/account_test.py
@@ -12,13 +12,17 @@ class TestAccountAPI(BaseTest):
         # test
         resp = self.create_user(user)
         resp_body = resp.json()
-        assert resp.status_code == 201
-        assert resp_body["username"] == user["userName"]
-        assert resp_body["userID"] != ""
+        try:
+            assert resp.status_code == 201
+            assert resp_body["username"] == user["userName"]
+            assert resp_body["userID"] != ""
+        except AssertionError:
+            raise
+        finally:
+            self.pprint_request(resp.request)
+            self.pprint_response(resp)
 
         # teardown
-        self.pprint_request(resp.request)
-        self.pprint_response(resp)
         # todo: need to delete the user
 
     data1 = [[{"userName": "user123", "password": "abcdefgh"}, 400, 26],
@@ -39,71 +43,105 @@ class TestAccountAPI(BaseTest):
 
     @pytest.mark.parametrize("user, status_code, len_", data1)
     def test_create_user_with_invalid_inputs(self, user, status_code, len_):
+        # setup
+
         # test
         resp = self.create_user(user)
-        assert resp.status_code == status_code
-        assert resp.text.find("Passwords must have at least one non alphanumeric character") == len_
+        try:
+            assert resp.status_code == status_code
+            assert resp.text.find("Passwords must have at least one non alphanumeric character") == len_
+        except AssertionError:
+            raise
+        finally:
+            self.pprint_request(resp.request)
+            self.pprint_response(resp)
 
         # teardown
-        self.pprint_request(resp.request)
-        self.pprint_response(resp)
 
     def test_delete_existing_user_basic_auth(self):
         # setup
         user = self.generate_username_password()
         resp = self.create_user(user)
-        assert resp.status_code == 201
+        try:
+            assert resp.status_code == 201
+        except AssertionError:
+            raise
+        finally:
+            self.pprint_request(resp.request)
+            self.pprint_response(resp)
         resp_body = resp.json()
 
         # test
         resp2 = self.delete_user_basic_auth(resp_body["userID"], user)
-        assert resp2.status_code == 204
+        try:
+            assert resp2.status_code == 204
+        except AssertionError:
+            raise
+        finally:
+            self.pprint_request(resp2.request)
+            self.pprint_response(resp2)
 
         # teardown
-        self.pprint_request(resp.request)
-        self.pprint_response(resp)
-        self.pprint_request(resp2.request)
-        self.pprint_response(resp2)
 
     def test_delete_existing_user_token(self):
         # setup
         user = self.generate_username_password()
         resp1 = self.create_user(user)
-        assert resp1.status_code == 201
+        try:
+            assert resp1.status_code == 201
+        except AssertionError:
+            raise
+        finally:
+            self.pprint_request(resp1.request)
+            self.pprint_response(resp1)
         resp_body1 = resp1.json()
         uuid_ = resp_body1["userID"]
         resp2 = self.generate_token(user)
-        assert resp2.status_code == 200
+        try:
+            assert resp2.status_code == 200
+        except AssertionError:
+            raise
+        finally:
+            self.pprint_request(resp2.request)
+            self.pprint_response(resp2)
         resp_body2 = resp2.json()
         token = resp_body2["token"]
 
         # test
         resp3 = self.delete_user_token(uuid_, token)
-        assert resp3.status_code == 204
+        try:
+            assert resp3.status_code == 204
+        except AssertionError:
+            raise
+        finally:
+            self.pprint_request(resp3.request)
+            self.pprint_response(resp3)
 
         # teardown
-        self.pprint_request(resp1.request)
-        self.pprint_response(resp1)
-        self.pprint_request(resp2.request)
-        self.pprint_response(resp2)
-        self.pprint_request(resp3.request)
-        self.pprint_response(resp3)
 
     def test_delete_user_with_invalid_uuid_basic_auth(self):
         # setup
         user = self.generate_username_password()
         resp1 = self.create_user(user)
-        assert resp1.status_code == 201
+        try:
+            assert resp1.status_code == 201
+        except AssertionError:
+            raise
+        finally:
+            self.pprint_request(resp1.request)
+            self.pprint_response(resp1)
         uuid_ = str(uuid.uuid4())
 
         # test
         resp2 = self.delete_user_basic_auth(uuid_, user)
-        assert resp2.status_code == 200
-        assert resp2.text == '{"code":"1207","message":"User Id not correct!"}'
+        try:
+            assert resp2.status_code == 200
+            assert resp2.text == '{"code":"1207","message":"User Id not correct!"}'
+        except AssertionError:
+            raise
+        finally:
+            self.pprint_request(resp2.request)
+            self.pprint_response(resp2)
 
         # teardown
-        self.pprint_request(resp1.request)
-        self.pprint_response(resp1)
-        self.pprint_request(resp2.request)
-        self.pprint_response(resp2)
         # todo: need to delete the user
